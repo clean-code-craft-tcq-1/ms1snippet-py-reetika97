c1a7c3ff73f3e772d02e9c4c8edc5d35f2706572
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-03-22 16:48:28 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,19 +1,15 @@
 
+maxDelta={'soc':0.05, 'current':0.1}
+
 def  _give_me_a_good_name(value, nextValue, maxDelta):
   if nextValue - value > maxDelta:
     return False
   return True
 
-def validate_soc_reading(values):
+def validate_reading(values, param):
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
+    if(not _give_me_a_good_name(values[i], values[i + 1], maxDelta[param])):
       return False
   return True
 
-def validate_current_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
-      return False
-  return True
