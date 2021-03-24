922bcf65c06ddddd41279e59f1ae288ee522dcb4
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-03-24 10:34:28 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,25 +1,15 @@
 
 maxDelta={'soc':0.05, 'current':0.1}
 
-#Error to see if values are not passed
-class ListEmptyError(Exception):
-    pass
-
 def readings_below_maxDelta(value, nextValue, maxDelta):
   if nextValue - value > maxDelta:
     return False
   return True
 
 def validate_reading(values, param):
-  try:
-    if(len(values)<=0):
-      raise ListEmptyError
-    last_but_one_reading = len(values) - 1
-    for i in range(last_but_one_reading):
-      if(not readings_below_maxDelta(values[i], values[i + 1], maxDelta[param])):
-        return False
-    return True
-  except ListEmptyError:
-    print("No values read!")
-    return
-
+  last_but_one_reading = len(values) - 1
+  for i in range(last_but_one_reading):
+    if(not readings_below_maxDelta(values[i], values[i + 1], maxDelta[param])):
+      return False
+  return True
+  
